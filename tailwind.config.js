c46a6d666ec7f9fa98d22582866048bdd33dce4c
realnifty <rogelioalbertopedraza@gmail.com>
2022-04-08 01:08:49 -0500

    add custom fonts and colors to tailwind.config, add icons to index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    colors:{
+      'msblue': '#304bf0',
+      'msred': '#e34d70',
+      'msdark': '#39393b',
+      'mslight': '#eef5ff',
+    },
+    extend: {
+      fontFamily: {
+        'fredoka': ['Fredoka One', 'cursive'],
+        'sawagoth': ['Sawarabi Gothic', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
